a71cf96c87b9e18d70c3437562e7cf770880fbd0
Maggie Perry <mperry@resulta.com>
2026-08-11 16:27:52 -0300

    7-mr: Created Animal repository mem

--- src/main/java/com/marinerescue/Main.java
+++ src/main/java/com/marinerescue/Main.java
@@ -1,60 +1,86 @@
 package com.marinerescue;
 
+import java.util.UUID;
+
 import com.marinerescue.model.*;
+import com.marinerescue.repository.InMemoryAnimalRepository;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Marine Rescue System Starting...");
 
+        // Define Animal Repo
+        InMemoryAnimalRepository repo = new InMemoryAnimalRepository();
+
         // Enter new animal
         Animal baby = new Animal("Baby", Species.HARBOUR_SEAL, 2);
         System.out.println(baby);
 
-        RescueCase case_one = new RescueCase(baby, "Halifax");
+        // Save to mem
+        repo.save(baby);
 
-        case_one.addObservation(new Observation("Poor baby got injured by a boat propeller", "Maggie"));
+        // Find by ID
+        System.out.println(repo.findById(baby.getID()));
+        // Random ID test
+        System.out.println(repo.findById(UUID.randomUUID()));
 
-        System.out.println(case_one);
+        // Find by species
+        System.out.println(repo.findBySpecies(Species.HARBOUR_SEAL));
+
+        // Find by all
+        System.out.println(repo.findByAll());
+
+        // Delete
+        repo.delete(baby.getID());
+        System.out.println(repo.findByAll()); // should be empty
+
+        // RescueCase case_one = new RescueCase(baby, "Halifax");
+
+        // case_one.addObservation(new Observation("Poor baby got injured by a boat
+        // propeller", "Maggie"));
+
+        // System.out.println(case_one);
 
         // Assigning
-        case_one.assignVolunteer(new Volunteer("Maggie", "902-880-1234", true));
-        System.out.println(case_one);
+        // case_one.assignVolunteer(new Volunteer("Maggie", "902-880-1234", true));
+        // System.out.println(case_one);
 
         // In rescue
-        case_one.advanceStatus();
-        System.out.println(case_one);
+        // case_one.advanceStatus();
+        // System.out.println(case_one);
 
         // Rehabilitating
-        case_one.advanceStatus();
-        case_one.addObservation(
-                new Observation("Baby is recovering well and should be ready to release soon", "Maggie"));
+        // case_one.advanceStatus();
+        // case_one.addObservation(
+        // new Observation("Baby is recovering well and should be ready to release
+        // soon", "Maggie"));
 
-        System.out.println(case_one);
+        // System.out.println(case_one);
 
         // Update health status
-        baby.setHealthStatus(HealthStatus.STABLE);
-        System.out.println(baby);
+        // baby.setHealthStatus(HealthStatus.STABLE);
+        // System.out.println(baby);
 
         // Released
-        case_one.advanceStatus();
-        case_one.addObservation(
-                new Observation("Baby is released!", "Maggie"));
+        // case_one.advanceStatus();
+        // case_one.addObservation(
+        // new Observation("Baby is released!", "Maggie"));
 
-        System.out.println(case_one);
+        // System.out.println(case_one);
 
         // Case two animal testing without adding volunteer or observations
-        Animal fuzzy = new Animal("Fuzzy", Species.PUFFIN, 1);
-        System.out.println(fuzzy);
+        // Animal fuzzy = new Animal("Fuzzy", Species.PUFFIN, 1);
+        // System.out.println(fuzzy);
 
-        RescueCase case_two = new RescueCase(fuzzy, "Cape Breton");
-        case_two.advanceStatus();
-        System.out.println(case_two);
-        case_two.advanceStatus();
-        System.out.println(case_two);
-        case_two.advanceStatus();
-        System.out.println(case_two);
-        case_two.advanceStatus();
-        System.out.println(case_two);
+        // RescueCase case_two = new RescueCase(fuzzy, "Cape Breton");
+        // case_two.advanceStatus();
+        // System.out.println(case_two);
+        // case_two.advanceStatus();
+        // System.out.println(case_two);
+        // case_two.advanceStatus();
+        // System.out.println(case_two);
+        // case_two.advanceStatus();
+        // System.out.println(case_two);
 
         // Try to set age to invalid value
         // baby.setAge(-1);
